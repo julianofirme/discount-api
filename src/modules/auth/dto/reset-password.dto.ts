08a5960941efbfb24785f9822edc50ee4c327eda
Juliano Firme <julianofirme1@gmail.com>
2022-06-19 12:51:07 -0300

    feat(swagger): create the swagger documentation

--- src/modules/auth/dto/reset-password.dto.ts
+++ src/modules/auth/dto/reset-password.dto.ts
@@ -1,12 +1,16 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsString } from 'class-validator';
 
 export class ResetPasswordDto {
+  @ApiProperty()
   @IsEmail()
   email: string;
 
+  @ApiProperty()
   @IsString()
   password: string;
 
+  @ApiProperty()
   @IsString()
   code: string;
 }
--- src/modules/product/dto/create-product.dto.ts
+++ src/modules/product/dto/create-product.dto.ts
@@ -1,18 +1,41 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNumber, IsOptional, IsString } from 'class-validator';
 
 export class CreateProductDto {
+  @ApiProperty({
+    description: 'Product name will be displayed in product data',
+    example: 'Shampoo Treseme',
+  })
   @IsString()
   name: string;
 
+  @ApiProperty({
+    description: 'Product description will be displayed in product data',
+    example: 'Shampoo hidratação profunda',
+  })
   @IsString()
   description: string;
 
+  @ApiProperty({
+    description:
+      'Product value will be displayed in product data, this field represents the value in cents',
+    example: 100000,
+  })
   @IsNumber()
   value: number;
 
+  @ApiProperty({
+    description: 'Product quantity will be displayed in product data',
+    example: 5,
+  })
   @IsNumber()
   quantity: number;
 
+  @ApiProperty({
+    description:
+      'Product logo will be displayed in product data, this field is a url from image src',
+    example: 'https://m.media-amazon.com/images/I/31wIIbQPWNS._AC_.jpg',
+  })
   @IsOptional()
   @IsString()
   logo: string;
